c761330b76d3a1bf02c8700396e6fa3d7ea272c2
eddihe <rok_ray@live.com>
2022-03-12 15:02:45 +0000

    add script python in example file print wheater of city layoun morocco

--- example.py
+++ example.py
@@ -1,3 +1,19 @@
-def git_opeeation():
-    print("I am adding example.py file to the remote repository.")
-git_opeation()
+#!/usr/bin python3
+import requests
+
+API_KEY = "ddd929482aa63c4dbb230c0a5fe1624e"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+city = "Laayoune"
+requests_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(requests_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273, 2)
+    humidity = round(data["main"]["humidity"], 2)
+    print('Weather', weather)
+    print("Temperature", temperature)
+    print("Humidity", humidity,"%")
+else:
+    print("An error occured")
